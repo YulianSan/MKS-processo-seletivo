4544d6df9a7c2324b6a849a97fa929455d6676cb
YulianSan <knoob0733@gmail.com>
2023-11-03 20:34:43 -0300

    feat: basic style container and flex containers

---
+++ src/styled/components/Container.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: var(--bg-primary);
+    width: 100%;
+    color: var(--text-primary);
+`
+
+export const FlexRow = styled.div`
+    display: flex;
+`
+
+export const FlexColumn = styled.div`
+    display: flex;
+    flex-direction: column;
+`
