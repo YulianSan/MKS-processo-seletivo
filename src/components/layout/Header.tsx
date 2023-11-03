cb6af05c07afe36ccf70c8bd2b9f97688cdf4c78
YulianSan <knoob0733@gmail.com>
2023-11-03 20:36:31 -0300

    feat: adding header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,1 +1,13 @@
+"use client";
+import * as S from "@/styled/layout/Header"
 
+export function Header() {
+    return (
+        <S.Container>
+            <S.ContainerTitle>
+                <S.Title>MKS</S.Title>
+                <S.SubTitle>Sistemas</S.SubTitle>
+            </S.ContainerTitle>
+        </S.Container>
+    )
+}
