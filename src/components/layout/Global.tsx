3ec278003402387353ed0dbd8e89d6da7b055903
YulianSan <knoob0733@gmail.com>
2023-11-03 20:33:27 -0300

    feat: adding global style

---
+++ src/components/layout/Global.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { GlobalStyled } from '@/styled/global'
+
+export function Global() {
+    return (
+        <GlobalStyled />
+    )
+}
---
+++ src/styled/global.ts
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+:root {
+    --bg-primary: #0F52BA;
+    --bg-secondary: #373737;
+    --text-primary: #FFFFFF;
+    --text-secondary: #2C2C2C;
+}
+`
