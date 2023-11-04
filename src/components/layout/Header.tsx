a9f601976a6c0c92f18110e42ff698295f04cb07
YulianSan <knoob0733@gmail.com>
2023-11-04 17:51:44 -0300

    feat: adding menu cart

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,18 +1,25 @@
 "use client";
 import { FaShoppingCart } from 'react-icons/fa'
 import * as S from "@/styled/layout/Header"
+import { useState } from 'react';
+import { MenuCart } from './MenuCart';
 
 export function Header() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <S.Container>
-            <S.ContainerTitle>
-                <h1>MKS</h1>
-                <span>Sistemas</span>
-            </S.ContainerTitle>
-            <S.CartButton>
-                <FaShoppingCart />
-                <span>0</span>
-            </S.CartButton>
-        </S.Container>
+        <>
+            <S.Container>
+                <S.ContainerTitle>
+                    <h1>MKS</h1>
+                    <span>Sistemas</span>
+                </S.ContainerTitle>
+                <S.CartButton onClick={() => setIsOpen(true)}>
+                    <FaShoppingCart />
+                    <span>0</span>
+                </S.CartButton>
+            </S.Container>
+            <MenuCart isOpen={ isOpen } close={() => setIsOpen(false)} />
+        </>
     )
 }
