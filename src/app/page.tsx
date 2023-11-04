aaee324cc9bf91caea046d80e7698690810f97a0
YulianSan <knoob0733@gmail.com>
2023-11-04 17:51:12 -0300

    feat: adding type in request

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,17 @@ import { Card } from "@/components/Card/Card";
 import * as S from "@/components/layout/Main";
 import { useProducts } from "@/hooks/UseProducts";
 
+interface IResponse {
+    products: IProduct[],
+    count: number
+}
+
 export default function Home() {
-  const { isLoading, data } = useProducts()
+  const { isLoading, data } = useProducts<IResponse>()
   return (
     <S.Main>
       <S.ListCards>
-        {!isLoading && data.products.map( card =>  
+        {!isLoading && data?.products.map( card =>  
           <Card key={card.id} 
             image={card.photo} 
             title={card.name} 
--- src/hooks/UseProducts.ts
+++ src/hooks/UseProducts.ts
@@ -1,8 +1,8 @@
 import { useQuery } from "react-query";
 
-export function useProducts() {
+export function useProducts<T = any>() {
 
-	const { isLoading, data } = useQuery<any, any>('products', () => {
+	const { isLoading, data } = useQuery<T, any>('products', () => {
 		return fetch('https://mks-frontend-challenge-04811e8151e6.herokuapp.com/api/v1/products?page=1&rows=8&sortBy=id&orderBy=DESC')
 			.then(e => e.json())
 	})
