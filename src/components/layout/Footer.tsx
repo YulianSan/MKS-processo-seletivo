bb7cfa6b0e3f975e8cab4edc121be4b80f96c7a0
YulianSan <knoob0733@gmail.com>
2023-11-04 16:32:48 -0300

    feat: adding footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,10 @@
+"use client";
+import * as S from '@/styled/layout/Footer'
+
+export function Footer() {
+    return (
+        <S.Container>
+            <span>MKS sistemas &copy; Todos os direitos reservados</span>
+        </S.Container>
+    )
+}
---
+++ src/styled/layout/Footer.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.footer`
+    margin-top: 5rem;
+    text-align: center;
+    background-color: var(--bg-tertiary);
+    padding-block: 0.5rem;
+    & span {
+        font-size: 0.75rem;
+        color: #000000;
+    }
+`
