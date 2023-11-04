efa26a21dd010e61eb750686a2a45d2471c92faf
YulianSan <knoob0733@gmail.com>
2023-11-04 15:18:31 -0300

    feat: showing products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import { Card } from "@/components/Card/Card";
+import * as S from "@/components/layout/Main";
+import { useProducts } from "@/hooks/UseProducts";
+
 export default function Home() {
+  const { isLoading, data } = useProducts()
   return (
-    <main>
-      oi
-    </main>
+    <S.Main>
+      <S.ListCards>
+        {!isLoading && data.products.map( card =>  
+          <Card key={card.id} 
+            image={card.photo} 
+            title={card.name} 
+            description={card.description}
+            price={card.price}/>,
+        )}
+      </S.ListCards>
+    </S.Main>
   )
 }
---
+++ src/components/layout/Main.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+    margin-top: 7.25rem;
+`
+
+export const ListCards = styled.div`
+    display: grid;
+    column-gap: 1.4rem;
+    row-gap: 2rem;
+    grid-template-columns: repeat(auto-fill, minmax(13.75rem, 1fr));
+    max-width: 60rem;
+    margin-inline: auto;
+`
