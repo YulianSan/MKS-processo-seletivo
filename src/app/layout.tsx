8b65cb674e64693a8a54a43c90b9cb58aacc92ba
YulianSan <knoob0733@gmail.com>
2023-11-04 16:33:50 -0300

    feat: using footer and adding font Montserrat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,9 @@
+import { Footer } from '@/components/layout/Footer'
 import { Global } from '@/components/layout/Global'
 import { Header } from '@/components/layout/Header'
 import { ReactQuery } from '@/contexts/ReactQuery'
 import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
-
-const montserrat = Montserrat({ subsets: ['latin'] })
+import Head from 'next/head'
 
 export const metadata: Metadata = {
   title: 'MKS - ',
@@ -18,13 +17,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR" className={montserrat.className}>
+    <html lang="pt-BR">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet" />
+      </Head>
       <Global />
       <body>
         <Header />
         <ReactQuery>
           { children }
         </ReactQuery>
+        <Footer />
       </body>
     </html>
   )
--- src/styled/global.ts
+++ src/styled/global.ts
@@ -5,10 +5,14 @@ export const GlobalStyled = createGlobalStyle`
     padding: 0;
     margin: 0;
     box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+    text-rendering: optimizeSpeed;
 }
+
 :root {
     --bg-primary: #0F52BA;
     --bg-secondary: #373737;
+    --bg-tertiary: #EEEEEE;
     --text-primary: #FFFFFF;
     --text-secondary: #2C2C2C;
     --radius: 0.5rem;
